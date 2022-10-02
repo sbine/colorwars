618bc4a8684f6c7dcd4230b48c4abff1eb7c17f7
Sara Bine <sara@sarabine.com>
2022-10-02 13:44:43 -0600

    Fix game over logic

--- src/game.js
+++ src/game.js
@@ -8,6 +8,7 @@ class Game {
 
     start() {
         this.turns = []
+        this.color = this.options.startingColor
         this.grid = new Grid({
             startingColor: this.options.startingColor,
             colors: this.options.colors,
@@ -26,10 +27,11 @@ class Game {
         const lastColor = this.color
         this.color = newColor
 
-        const newScore = this.grid.setColor(newColor)
-        this.turns.push(new Turn(lastColor, newColor, newScore))
+        const tilesChanged = this.grid.setColor(newColor)
+        const newTiles = this.grid.newTiles.length || 0
+        this.turns.push(new Turn(lastColor, newColor, newTiles))
 
-        if (newScore == this.options.columns * this.options.rows) {
+        if (tilesChanged + newTiles == this.options.columns * this.options.rows) {
             this.gameOver = true
 
             if (!this.highScore || this.turns.length < this.highScore) {
